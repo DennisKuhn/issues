675f214134ae1e4b41073c46daff894285279992
= <=>
2019-06-12 17:15:52 -0700

    Prop Controls #186

---
+++ property-controls-186/src/webparts/dateTimePickerOnPropertyPaneFieldChanged/components/IDateTimePickerOnPropertyPaneFieldChangedProps.ts
@@ -0,0 +1,5 @@
+import { IDateTimeFieldValue } from '@pnp/spfx-property-controls/lib/PropertyFieldDateTimePicker';
+
+export interface IDateTimePickerOnPropertyPaneFieldChangedProps {
+  datetime: IDateTimeFieldValue;
+}
